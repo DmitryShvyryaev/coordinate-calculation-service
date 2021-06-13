07722ac4f30d23da8dc479945667dbb6d276309b
DmitryShvyryaev <DmitryShvuryaev@yandex.ru>
2021-06-13 15:39:44 +0300

    create_project

---
+++ src/main/java/ru/dshvyryaev/ccs/CoordinateCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.dshvyryaev.ccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinateCalculationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoordinateCalculationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dshvyryaev/ccs/CoordinateCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dshvyryaev.ccs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinateCalculationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
